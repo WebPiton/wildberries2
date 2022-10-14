0c0d9247b8f87518023fb8bd77eb60656a584de5
WebPiton <62644864+WebPiton@users.noreply.github.com>
2022-10-14 21:09:15 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("#form");
+const btn = document.getElementById("buttonHeader");
+const fields = document.querySelectorAll(".fields");
+
+btn.addEventListener("click", scrollButtonClick);
+
+function scrollButtonClick() {
+	form.scrollIntoView(true, { behavior: "smooth" });
+}
+
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const formData = new FormData(form);
+
+	let response = await fetch("http://localhost:9999/feedback", {
+		method: "POST",
+		body: formData,
+	});
+
+	if (response.status === 200) {
+		alert("Ок");
+	}
+});
